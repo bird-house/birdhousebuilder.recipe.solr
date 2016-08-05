e9fa252f6a5ebd94c567e3237edf940b178b7df4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-05 17:27:14 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ This module contains the tool of birdhousebuilder.recipe.solr
 from setuptools import find_packages
 from setuptools import setup
 
+name = 'birdhousebuilder.recipe.solr'
+
 version = '0.2.0'
 description = 'A Buildout recipe to install and configure Apache Solr with Anaconda.'
 long_description = (
@@ -13,18 +15,22 @@ long_description = (
     open('CHANGES.rst').read()
 )
 
-entry_point = 'birdhousebuilder.recipe.solr'
-entry_points = {"zc.buildout": [
-                            "default = %s:Recipe" % entry_point,
-                          ],
-                "zc.buildout.uninstall": [
-                            "default = %s:uninstall" % entry_point,
-                          ],
-                       }
+entry_points = '''
+[zc.buildout]
+default = %(name)s:Recipe
+[zc.buildout.uninstall]
+default = %(name)s:uninstall
+''' % globals()
 
-tests_require = ['zc.buildout']
+reqs = ['setuptools',
+        'zc.buildout',
+        'zc.recipe.deployment >=1.3.0',
+        'Mako',
+        'birdhousebuilder.recipe.conda >=0.3.3',
+        'birdhousebuilder.recipe.supervisor >=0.3.4',]
+tests_reqs = ['zc.buildout', 'zope.testing']
 
-setup(name='birdhousebuilder.recipe.solr',
+setup(name=name,
       version=version,
       description=description,
       long_description=long_description,
@@ -38,21 +44,14 @@ setup(name='birdhousebuilder.recipe.solr',
       ],
       keywords='buildout recipe solr birdhouse conda anaconda',
       author='Birdhouse',
+      author_email='wps-dev at dkrz.de',
       url='https://github.com/bird-house/birdhousebuilder.recipe.solr',
-      license='BSD',
+      license='Apache License 2',
+      install_requires = reqs,
+      extras_require = dict(tests=tests_reqs),
+      entry_points=entry_points,
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['birdhousebuilder', 'birdhousebuilder.recipe'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
-                        'zc.buildout',
-                        # -*- Extra requirements: -*-
-                        'Mako',
-                        'birdhousebuilder.recipe.conda',
-                        'birdhousebuilder.recipe.supervisor',
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='birdhousebuilder.recipe.solr.tests.test_docs.test_suite',
-      entry_points=entry_points,
       )
