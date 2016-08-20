b3b3902a02b586445a57d1c18f293826d0fa7534
Pingu Carsti <pingu@adele.local>
2016-08-20 15:17:27 +0200

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 name = 'birdhousebuilder.recipe.solr'
 
-version = '0.2.0'
+version = '0.2.1'
 description = 'A Buildout recipe to install and configure Apache Solr with Anaconda.'
 long_description = (
     open('README.rst').read() + '\n' +
